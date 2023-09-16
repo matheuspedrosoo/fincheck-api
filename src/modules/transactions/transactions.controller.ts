49a74399ce7c731ec787cf47d422cd2183b7d707
Matheus Pedroso <matheuspedroso404@gmail.com>
2023-09-16 16:47:02 -0300

    delete console.log

--- src/modules/transactions/transactions.controller.ts
+++ src/modules/transactions/transactions.controller.ts
@@ -42,7 +42,6 @@ export class TransactionsController {
     @Query('type', new OptionalParseEnumPipe(TransactionType))
     type?: TransactionType,
   ) {
-    console.log({ bankAccountId });
     return this.transactionsService.findAllByUserId(userId, {
       month,
       year,
