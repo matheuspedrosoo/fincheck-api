1092437d4a082ce19a02e8e3c0ee94caa2c0bfea
Matheus Pedroso <matheuspedroso404@gmail.com>
2023-11-05 18:32:01 -0300

    feat: fix import OptionalParseEnumPipe

--- src/modules/transactions/transactions.controller.ts
+++ src/modules/transactions/transactions.controller.ts
@@ -17,7 +17,7 @@ import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
 import { OptionalParseUUIDPipe } from './../../shared/pipes/OptionalParseUUIDPipe';
 import { TransactionType } from './entities/Transaction';
-import { OptionalParseEnumPipe } from 'src/shared/pipes/OptionalParseEnumPipe';
+import { OptionalParseEnumPipe } from '../../shared/pipes/OptionalParseEnumPipe';
 import { ActiveUserId } from '../../shared/decorators/ActiveUserId';
 
 @Controller('transactions')
