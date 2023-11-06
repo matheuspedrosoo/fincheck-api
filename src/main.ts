6565f7e0d3eb3b15225b3f3c47ab8168e5532e43
Matheus Pedroso <matheuspedroso404@gmail.com>
2023-11-05 21:58:00 -0300

    chore: remove cors

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,14 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
-  // app.enableCors({
-  //   origin: '*',
-  // });
-  app.use(
-    cors({
-      origin: '*',
-    }),
-  );
+  app.enableCors({
+    origin: '*',
+  });
 
   await app.listen(process.env.PORT, '0.0.0.0');
 }
