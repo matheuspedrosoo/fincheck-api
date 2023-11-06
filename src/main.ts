82041bdbc089a29b3a0e4ff774cf804b0cb6fa14
Matheus Pedroso <matheuspedroso404@gmail.com>
2023-11-05 21:42:22 -0300

    chore: install cors

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,20 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: '*',
-  });
+  // app.enableCors({
+  //   origin: '*',
+  // });
+  app.use(
+    cors({
+      origin: '*',
+    }),
+  );
 
   await app.listen(process.env.PORT, '0.0.0.0');
 }
